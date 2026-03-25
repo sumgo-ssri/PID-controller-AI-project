db5090ae252bbe9f975f0b70338bee5902677f91
unknown <lwj08062403@gmail.com>
2026-03-25 23:48:15 +0900

    initial commit

---
+++ src/main/java/com/develop/capstoneproject/CapstoneprojectApplication.java
@@ -0,0 +1,13 @@
+package com.develop.capstoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneprojectApplication.class, args);
+    }
+
+}
