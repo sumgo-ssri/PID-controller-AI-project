adbafeddf2cdec9db30cbd0e562595c3335996da
윤준환 <yunjunhwan2@gmail.com>
2026-03-30 17:14:32 +0900

    feat: feedback page organize

--- src/main/java/com/develop/capstoneproject/feedback/FeedbackRepository.java
+++ src/main/java/com/develop/capstoneproject/feedback/FeedbackRepository.java
@@ -4,6 +4,11 @@ package com.develop.capstoneproject.feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
+    //로그창에서 피드백 내용 뽑아갈 때
+    List<Feedback> findAllByOrderByCreatedAtDesc();
 }
