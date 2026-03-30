a810b4c0c19694594a108a0fd9adca46c00c228f
윤준환 <yunjunhwan2@gmail.com>
2026-03-30 22:53:26 +0900

    feat:oneHour later data automatically delete

--- src/main/java/com/develop/capstoneproject/CapstoneprojectApplication.java
+++ src/main/java/com/develop/capstoneproject/CapstoneprojectApplication.java
@@ -2,7 +2,9 @@ package com.develop.capstoneproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CapstoneprojectApplication {
 
--- src/main/java/com/develop/capstoneproject/feedback/FeedbackRepository.java
+++ src/main/java/com/develop/capstoneproject/feedback/FeedbackRepository.java
@@ -2,8 +2,12 @@ package com.develop.capstoneproject.feedback;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -11,4 +15,9 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 
     //로그창에서 피드백 내용 뽑아갈 때
     List<Feedback> findAllByOrderByCreatedAtDesc();
+
+    //1시간 지난 데이터 삭제하기
+     @Modifying
+    @Query("DELETE FROM Feedback f WHERE f.createdAt <:time")
+    void deleteOldFeedbacks(@Param("time") LocalDateTime time);
 }
